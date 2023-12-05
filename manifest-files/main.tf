eef603f1d533bdda69fafa2a0329ec3df04a2006
shola-adegunle <shola.adegunle@yahoo.com>
2023-12-05 13:16:05 +0000

    backend config removed

--- manifest-files/main.tf
+++ manifest-files/main.tf
@@ -5,13 +5,13 @@ terraform {
       version = ">=5.0"
     }
   }
-  backend "gcs" {
-  bucket = "tf-state-sadey2k"
-  }
+  #backend "gcs" {
+  #bucket = "tf-state-sadey2k"
+
 }
 
 provider "google" {
-  credentials = file("E:/Study/DevOps/Azure_DevOps_Courses/TERRAFORM/LABS/GCP/key.json")
+  #credentials = file("E:/Study/DevOps/Azure_DevOps_Courses/TERRAFORM/LABS/GCP/key.json")
   project     = "lab01-gcp-sadey2k"
   region      = "europe-west2" # Default region for resources
 }
