cfc6a4f3defb5b401d2e74ea408daf70d2f52e99
shola-adegunle <shola.adegunle@yahoo.com>
2023-12-05 19:04:34 +0000

    updated backend

--- manifest-files/main.tf
+++ manifest-files/main.tf
@@ -5,13 +5,15 @@ terraform {
       version = ">=5.0"
     }
   }
-  #backend "gcs" {
-  #bucket = "tf-state-sadey2k"
-
+  backend "gcs" {
+    bucket  = "tf-state-sadey2k"
+    prefix  = "dev/tf-state"
+  }
 }
 
 provider "google" {
-  #credentials = file("E:/Study/DevOps/Azure_DevOps_Courses/TERRAFORM/LABS/GCP/key.json")
+  // The credentials line is commented out because you will use a service connection in Azure DevOps.
+  // credentials = file("E:/Study/DevOps/Azure_DevOps_Courses/TERRAFORM/LABS/GCP/key.json")
   project     = "lab01-gcp-sadey2k"
-  region      = "europe-west2" # Default region for resources
+  region      = "europe-west2"  // Default region for resources
 }
